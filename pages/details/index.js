dc408b61b11b781039b42da6dabd8b394ea53d15
harddownloader <serafimdev@gmail.com>
2022-06-16 16:59:58 +0300

    update order page (rewtire to material style)

--- pages/details/index.js
+++ pages/details/index.js
@@ -1,71 +1,85 @@
-import Layout from "../../components/Layout";
-import React, { Component } from "react";
-import {
-  Row,
-  Col,
-  Card,
-  Input,
-  Select,
-  AutoComplete,
-  InputNumber,
-  Button
-} from "antd";
+import React, { useState, useEffect } from "react";
 import Swal from "sweetalert2";
 import Router from "next/router";
 import { dec } from "../../utils/encdec";
 import { postBookSeat } from "../../actions/book";
-const { Option } = Select;
 import Header from '../../components/HeaderMaterial/Header';
+// import Container from '@mui/material/Container';
 // import Grid from '@mui/material/Grid';
 // import IconButton from '@mui/material/IconButton';
 // import Button from '@mui/material/Button';
-// import ConfirmModal from '@/components/Dialog/ConfirmModal'
+import ConfirmModal from '@/components/Dialog/ConfirmModal'
+import PoperCard from '@/components/Card/PoperCard'
+// import Select from "@mui/material/Select";
+// import Autocomplete from "@mui/material/Autocomplete";
+// import Box from '@mui/material/Box';
+// import Typography from '@mui/material/Typography';
+// import Paper from '@mui/material/Paper';
+import {
+  // Input,
+  TextField,
+  Paper,
+  Typography,
+  Box,
+  Autocomplete,
+  // Select,
+  Button,
+  IconButton,
+  Grid,
+  Container,
+  MenuItem,
+  InputLabel,
+} from '@mui/material';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import classes from './Details.module.scss'
 
+// class Details extends React.Component {
+function Details(props) {
+  const [dataSource, setDataSource] = useState([])
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+  const [address, setAdress] = useState('lipoviy_adress')
 
-class Details extends React.Component {
-  state = {
-    dataSource: [],
-    name: "",
-    email: "",
-    phone: "",
-    address: ""
+  const handleAutoComplete = value => {
+    setDataSource(
+      !value || value.indexOf("@") >= 0
+        ? []
+        : [
+            `${value}@gmail.com`,
+            `${value}@outlook.com`,
+            `${value}@yahoo.com`
+          ]
+    )
+    setEmail(value)
   };
 
-  handleAutoComplete = value => {
-    this.setState({
-      dataSource:
-        !value || value.indexOf("@") >= 0
-          ? []
-          : [
-              `${value}@gmail.com`,
-              `${value}@outlook.com`,
-              `${value}@yahoo.com`
-            ],
-      email: value
-    });
+  // const handleChange = e => {
+  //   this.setState({ [e.target.name]: e.target.value });
+  // };
+
+  const handleNumber = value => {
+    setPhone(value)
   };
 
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-  handleNumber = value => {
-    this.setState({ phone: value });
-  };
-
-  handleSubmit = async () => {
-    const { name, phone, address, email } = this.state;
-    const seatNumber = this.props.seat;
-    const info = { name, phone, address, email, seatNumber };
-    const resp = await postBookSeat(this.props.slug, info);
+  const handleSubmit = async () => {
+    const seatNumber = props.seat;
+    const info = {
+      name,
+      phone,
+      address,
+      email,
+      seatNumber
+    };
+    const resp = await postBookSeat(props.slug, info);
     if (!resp.error) {
-      this.sweetAlert("success");
+      sweetAlert("success");
     } else {
-      this.sweetAlert("error");
+      sweetAlert("error");
     }
   };
 
-  sweetAlert = status => {
+  const sweetAlert = status => {
     setTimeout(() => {
       if(status !== "error"){
         Router.push("/");
@@ -83,106 +97,159 @@ class Details extends React.Component {
     }
   };
 
-  render() {
-    return (
-      <Layout>
-        <Header />
-        <Row className="row-container">
-          {console.log(this.state)}
-          <Col span={4}></Col>
-          <Col span={8}>
-            <Card title="Ваши данные" style={{ width: "100%" }}>
-              <Input.Group>
-                <h4>Ваше имя:</h4>
-                <Input onChange={this.handleChange} name="name" />
-              </Input.Group>
-              <br />
-              <Input.Group>
-                <h4>Email:</h4>
-                <AutoComplete
-                  dataSource={this.state.dataSource}
-                  style={{ width: "100%" }}
-                  onChange={this.handleAutoComplete}
-                />
-              </Input.Group>
-              <Input.Group>
-                <h4>Адрес:</h4>
-                <Input onChange={this.handleChange} name="address" />
-              </Input.Group>
-              <br />
-              <Row>
-                <Col span={11}>
-                  <Input.Group>
-                    <h4>Телефон: </h4>
-                    <InputNumber
-                      style={{ width: "100%" }}
-                      onChange={this.handleNumber}
-                      name="phone"
-                      max={9999999999}
-                    />
-                  </Input.Group>
-                </Col>
-                <Col span={2}></Col>
-                <Col span={11}>
-                  <Input.Group>
-                    <h4>Пункт посадки: </h4>
-                    <Select defaultValue="Buspark" style={{ width: "100%" }}>
-                      <Option disabled value="Buspark">
-                        Одесса
-                      </Option>
-                      <Option disabled value="Attariya">
-                        Киев
-                      </Option>
-                    </Select>
-                  </Input.Group>
-                </Col>
-              </Row>
-              <br />
-              <Button
-                type="primary"
-                style={{ width: "100%" }}
-                onClick={this.handleSubmit}
+  return (
+    <>
+    <Header isDarkStyle containerWidth={"lg"} />
+    <Container maxWidth="lg">
+      
+      <Grid
+        container
+        spacing={2}
+      >
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={6}
+          lg={6}
+        >
+          <Paper
+            elevation={1}
+            className={classes.card}
+          >
+            <Typography variant="h5" gutterBottom component="div">
+              Ваши данные
+            </Typography>
+            <Grid
+              container
+              spacing={2}
+            >
+              <Grid
+                item
+                xs={12}
+                sm={12}
+                md={6}
+                lg={6}
               >
-                Подтвердить покупку
-              </Button>
-            </Card>
-          </Col>
-          <Col span={2}></Col>
-          <Col span={6}>
-            <Card title="Информация о рейсе" style={{ width: "100%" }}>
-              <p>
-                <b>Рейс: </b>
-                {this.props.start} - {this.props.end}
-              </p>
-              <p>
-                <b>Дата: </b>
-                {this.props.journeyDate}
-              </p>
-              <p>
-                <b>Места: </b>
-                {this.props.seat}
-              </p>
-              {/* <p>
-                <b>Travel: </b>
-                {this.props.travelName}
-              </p> */}
-            </Card>
+                <TextField
+                  id="name"
+                  className={`${classes.name_field} ${classes.order_field}`}
+                  label="Ваше Имя"
+                  variant="outlined"
+                  fullWidth
+                  onChange={e => setName(e.target.value)}
+                />
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                sm={12}
+                md={6}
+                lg={6}
+              >
+                <TextField
+                  id="phone"
+                  className={`${classes.phone_field} ${classes.order_field}`}
+                  label="Ваш Телефон"
+                  variant="outlined"
+                  fullWidth
+                  onChange={e => handleNumber(e.target.value)}
+                />
+              </Grid>
 
-            <br />
-            <Card title="Платежная информация" style={{ width: "100%" }}>
-              <p>
-                <b>Стоимость билета: </b> {this.props.fare} грн.
-              </p>
-              <p>
-                <b>Итого: </b> {this.props.fare} грн.
-              </p>
-            </Card>
-          </Col>
-          <Col span={4}></Col>
-        </Row>
-      </Layout>
-    );
-  }
+              <Grid
+                item
+                xs={12}
+                sm={12}
+                md={12}
+                lg={12}
+              >
+                <TextField
+                  name="email"
+                  id="email"
+                  className={`${classes.email_field} ${classes.order_field}`}
+                  label="Ваш Email"
+                  type="email"
+                  fullWidth
+                  onChange={e => setEmail(e.target.value)}
+                />
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                sm={12}
+                md={12}
+                lg={12}
+              >
+                <Button
+                  size="large"
+                  fullWidth
+                  variant="contained"
+                  onClick={handleSubmit}
+                >
+                  Подтвердить покупку
+                </Button>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={6}
+          lg={6}
+        >
+          <PoperCard
+            heading={"Информация о рейсе"}
+            content={[
+              {
+                title: "Рейс",
+                text: `${props.start} - ${props.end}`
+              },
+              {
+                title: "Дата",
+                text: `${props.journeyDate}`
+              },
+              {
+                title: "Места",
+                text: `${props.seat}`
+              },
+            ]}
+          />
+
+          <PoperCard
+            heading={"Платежная информация"}
+            content={[
+              {
+                title: "Стоимость билета",
+                text: `${props.fare}`
+              },
+              {
+                title: "Итого",
+                text: `${props.fare}`
+              }
+            ]}
+          />
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={6}
+          lg={6}
+        ></Grid>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={4}
+          lg={4}
+        ></Grid>
+      </Grid>
+    </Container>
+    </>
+  );
 }
 
 Details.getInitialProps = ({ query }) => {
