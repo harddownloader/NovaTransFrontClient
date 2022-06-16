cf80465469ff899d9e2e45928ea39d7f8457fdfd
harddownloader <serafimdev@gmail.com>
2022-06-16 14:12:57 +0300

    add PoperCard Component (we use it in deatails(order) page)

---
+++ components/Card/PoperCard.js
@@ -0,0 +1,24 @@
+import React from "react"
+import {
+Paper,
+Typography,
+} from '@mui/material'
+import classes from './PoperCard.module.scss'
+
+export default function PoperCard(props) {
+  const { heading, content } = props
+
+  return (
+    <Paper
+      elevation={1}
+      className={classes.poper_card}>
+      <Typography variant="h5" gutterBottom component="div">
+        {heading}
+      </Typography>
+
+      {content.map((textLine) => <Typography variant="body1" gutterBottom component="div">
+        <b>{textLine.title}: </b>{textLine.text}
+      </Typography>)}
+    </Paper>
+  )
+} 
