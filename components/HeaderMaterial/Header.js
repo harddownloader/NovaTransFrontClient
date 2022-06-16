1b970e2e58c4c6a8fa906cef12c42b22d05a7490
harddownloader <serafimdev@gmail.com>
2022-06-16 17:06:29 +0300

    update header props and styles

--- components/HeaderMaterial/Header.js
+++ components/HeaderMaterial/Header.js
@@ -3,74 +3,40 @@ import React from "react";
 // import { makeStyles } from "@mui/material/styles";
 import { makeStyles } from '@mui/styles';
 // import CssBaseline from '@mui/material/CssBaseline';
-
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 // import DirectionsBus from '@mui/icons-material/DirectionsBus';
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Container from '@mui/material/Container';
-
 // icons
 import HelpIcon from "@mui/icons-material/Help";
-
-// styles
-import styles from "@/styles/Header.module.scss";
-
+import classes from "./Header.module.scss";
 import { API_ROOT } from "../../utils/config";
 // images
 const logo = "/static/img/logos/logo.png";
 
 const useStyles = makeStyles((theme) => {
-  // console.log('theme', theme)
   return {
-  container: {
-    padding: "10px 0",
-    color: '#fff'
-  },
-  icon: {
-    marginRight: theme.spacing(2),
-  },
-  rightToolbar: {
-    marginLeft: "auto",
-    marginRight: -12,
-    display: "flex",
-    alignItems: "center",
-  }}
-  // menuButton: {
-  //   marginRight: 16,
-  //   marginLeft: -12
-  // },
-  // heroContent: {
-  //   backgroundColor: theme.palette.background.paper,
-  //   padding: theme.spacing(8, 0, 6),
-  // },
-  // heroButtons: {
-  //   marginTop: theme.spacing(4),
-  // },
-  // cardGrid: {
-  //   paddingTop: theme.spacing(8),
-  //   paddingBottom: theme.spacing(8),
-  // },
-  // card: {
-  //   height: '100%',
-  //   display: 'flex',
-  //   flexDirection: 'column',
-  // },
-  // cardMedia: {
-  //   paddingTop: '56.25%', // 16:9
-  // },
-  // cardContent: {
-  //   flexGrow: 1,
-  // },
-  // footer: {
-  //   backgroundColor: theme.palette.background.paper,
-  //   padding: theme.spacing(6),
-  // },
+    icon: {
+      marginRight: theme.spacing(2),
+    },
+    rightToolbar: {
+      marginLeft: "auto",
+      marginRight: -12,
+      display: "flex",
+      alignItems: "center",
+    }
+  }
 });
 
 const Header = (props) => {
-  const classes = useStyles();
+  const {
+    isDarkStyle,
+    containerWidth="md",
+  } = props
+
+  const styles = useStyles()
   
   return (
     <React.Fragment>
@@ -79,21 +45,21 @@ const Header = (props) => {
         color="transparent"
         elevation={0}
         position="relative"
-        className={classes.container}
+        className={`${classes.container} ${isDarkStyle ? classes.darkColors : classes.lightColors}`}
       >
-      <Container maxWidth="md">
+      <Container maxWidth={containerWidth}>
         <Toolbar>
           <img
             src={logo}
             alt="Logo NovaTrans"
             height={50}
-            className={classes.icon}
+            className={styles.icon}
           />
           <h1>NovaTrans</h1>
           {/* <Typography variant="h6" color="inherit" noWrap>
           NovaTrans
         </Typography> */}
-          <section className={classes.rightToolbar}>
+          <section className={styles.rightToolbar}>
             {/* <IconButton color="inherit" aria-label="Edit">
             <EditIcon />
           </IconButton>
