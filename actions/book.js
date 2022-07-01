eb7dcf52404515dd4af1d7e09c9054aeb95d9fb0
harddownloader <serafimdev@gmail.com>
2022-07-01 16:10:21 +0300

    update fetches to API (for multiply ticket order)

--- actions/book.js
+++ actions/book.js
@@ -1,9 +1,20 @@
-import fetch from "isomorphic-unfetch";
-import { API } from "../utils/config";
+import fetch from "isomorphic-unfetch"
+import { API } from "../utils/config"
 
-export const postSoldSeat = (slug, seat) =>
-  axios.post(`/bookings/sold/${slug}`, { seatNumber: seat });
+// slug - string
+// seats - array of strings
+export const postSoldSeat = (slug, seats) => {
+  return axios.post(`/bookings/sold/${slug}`, { seatNumber: JSON.stringify(seats) })
+}
 
+// slug - string
+// body : {
+//    name: string
+//    phone: string
+//    address: string
+//    email: string
+//    seatNumber: array(of strings) like string(because JSON.stringify(seatNumbers) )
+// }
 export const postBookSeat = async (slug, body) => {
   const resp = await fetch(`${API}/bookings/book/${slug}`, {
     method: "POST",
@@ -11,7 +22,7 @@ export const postBookSeat = async (slug, body) => {
       "Content-Type": "application/json"
     },
     body: JSON.stringify(body)
-  });
-  const response = await resp.json();
-  return response;
-};
+  })
+  const response = await resp.json()
+  return response
+}
