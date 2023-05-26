30d35801201b76cdf4a1a36e152ccfa292dcfd1b
harddownloader <serafimdev@gmail.com>
2023-05-27 02:14:52 +0300

    added shot down page: added functional for show only "WEBSITE NOT AVAILABLE" page

---
+++ pages/site_is_down.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export const WebsiteDownPage = () => {
+  return (
+    <>
+      <h1>Вибачте, сайт тимчасово недоступний. Ведуться технічні роботи.</h1>
+    </>
+  )
+}
+
+export default WebsiteDownPage
