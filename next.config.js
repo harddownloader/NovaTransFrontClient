ead42520cf1901699da2ffdbf98dceea92bd129e
harddownloader <serafimdev@gmail.com>
2023-06-01 03:39:57 +0300

    added and init sentry

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+const { withSentryConfig } = require('@sentry/nextjs');
+
 // next.config.js
 const path = require('path')
 const withPlugins = require('next-compose-plugins');
@@ -99,3 +105,9 @@ module.exports = withPlugins([
   // }],
 
 ], nextConfig);
+
+module.exports = withSentryConfig(
+  module.exports,
+  { silent: true },
+  { hideSourcemaps: true },
+);
