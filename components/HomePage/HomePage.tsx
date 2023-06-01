f09295f5a808782d4fc10f63b3fabe23a2ed1ba7
harddownloader <serafimdev@gmail.com>
2023-06-02 00:07:21 +0300

    fix deploy v7 - try loading SearchTickets thought dynamic next import

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import dynamic from 'next/dynamic';
+
 
 // mui
 import CssBaseline from "@mui/material/CssBaseline"
 
 // project components
 import Header from "@/components/HeaderMaterial/Header"
-import SearchTickets from "@/components/HomePage/SearchTickets/SearchTickets"
+// import SearchTickets from "@/components/HomePage/SearchTickets/SearchTickets"
 import WhyAreWe from "@/components/HomePage/WhyAreWe"
 import AboutDrivers from "@/components/HomePage/AboutDrivers"
 import Footer from "@/components/Footer/Footer"
+
+const SearchTickets = dynamic(() => import("@/components/HomePage/SearchTickets/SearchTickets"), {
+  loading: () => <p>Loading...</p>,
+});
 
 export const HomePage = () => {
   return (
@@ -18,7 +24,7 @@ export const HomePage = () => {
 
       <main>
         <h2>HomePage component</h2>
-        {/*<SearchTickets />*/}
+        <SearchTickets />
         <WhyAreWe />
         {/*<PopularTrips />*/}
         <AboutDrivers />
