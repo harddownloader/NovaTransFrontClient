acb82b8116c7188cb3a462dcfbec08995fd1ec59
harddownloader <serafimdev@gmail.com>
2023-06-01 19:42:12 +0300

    updated next config: fix console issues, migration to correct configuration - rm next-compose-plugins and next-images, added @svgr/webpack

--- next.config.js
+++ next.config.js
@@ -1,69 +1,48 @@
+/** @type {import('next').NextConfig} */
+const path = require('path')
+
 // This file sets a custom webpack configuration to use your Next.js app
 // with Sentry.
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 const { withSentryConfig } = require('@sentry/nextjs');
 
-// next.config.js
-const path = require('path')
-const withPlugins = require('next-compose-plugins');
-const withImages = require('next-images');
+const apiURL = new URL(process.env.NEXT_PUBLIC_API)
+const allowedImageDomains = process.env.NEXT_PUBLIC_ALLOWED_IMAGE_DOMAINS
+  ? process.env.NEXT_PUBLIC_ALLOWED_IMAGE_DOMAINS.split(",")
+  : []
+const imageConversionFormats = process.env.NEXT_PUBLIC_IMAGE_CONVERSION_FORMATS
+  ? process.env.NEXT_PUBLIC_IMAGE_CONVERSION_FORMATS.split(",")
+  : []
 
-// next.js configuration
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // useFileSystemPublicRoutes: false,
-  // distDir: 'build',
-  serverless: true,
-  // webpack5
-  webpack5: true,
-
-  // images
+  reactStrictMode: true,
   images: {
-    domains: ['localhost'],
+    domains: [apiURL.hostname, ...allowedImageDomains],
+    formats: imageConversionFormats,
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    dangerouslyAllowSVG: true,
   },
-  
-  // sass
   sassOptions: {
-    cssModules: true,
     includePaths: [path.join(__dirname, 'styles')],
   },
+  webpack(config, { isServer }) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
 
-  // webpack config
-  webpack: (config, options) => {
+    if (!isServer) {
+      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+      config.resolve.fallback = {
+        fs: false
+      }
+    }
 
-    const { isServer } = options;
-    // config.module.rules.push({
-    //   test: /\.(ogg|mp3|wav|mpe?g)$/i,
-    //   exclude: config.exclude,
-    //   use: [
-    //     {
-    //       loader: require.resolve('url-loader'),
-    //       options: {
-    //         limit: config.inlineImageLimit,
-    //         fallback: require.resolve('file-loader'),
-    //         publicPath: `${config.assetPrefix}/_next/static/img/`,
-    //         outputPath: `${isServer ? '../' : ''}static/img/`,
-    //         name: '[name]-[hash].[ext]',
-    //         esModule: config.esModule || false,
-    //       },
-    //     },
-    //   ],
-    // },
-    // {
-    //   test: /\.(png|jpg|gif)$/i,
-    //   use: [
-    //     {
-    //       loader: 'url-loader',
-    //       options: {
-    //         limit: 8192,
-    //       },
-    //     },
-    //   ],
-    // },
-    // );
-
-
-    return config;
+    return config
   },
 
   // redirects
@@ -90,24 +69,12 @@ const nextConfig = {
   //
   //   return redirectsRules;
   // }
-};
+}
 
-module.exports = withPlugins([
-
-  // add a plugin without a configuration
-  withImages,
-
-  // another plugin with a configuration
-  // [typescript, {
-  //   typescriptLoaderOptions: {
-  //     transpileOnly: false,
-  //   },
-  // }],
-
-], nextConfig);
+module.exports = nextConfig
 
 module.exports = withSentryConfig(
   module.exports,
   { silent: true },
   { hideSourcemaps: true },
-);
+)
