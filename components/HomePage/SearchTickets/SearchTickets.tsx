1bf0fef33da1af91e2c0e79977a6aeeb2882860a
harddownloader <serafimdev@gmail.com>
2023-06-07 01:01:07 +0300

    fix deploy v14 - SearchTickets mock - uncomment render v1.7 - one way fields - added datapicker simple version

--- components/HomePage/SearchTickets/SearchTickets.tsx
+++ components/HomePage/SearchTickets/SearchTickets.tsx
@@ -24,6 +24,11 @@ import {
 import SearchIcon from '@mui/icons-material/Search'
 import NorthEastOutlinedIcon from '@mui/icons-material/NorthEastOutlined'
 import SouthEastOutlinedIcon from '@mui/icons-material/SouthEastOutlined'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker'
+import { CalendarToday } from "@mui/icons-material"
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns"
+import ruLocale from "date-fns/locale/ru"
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider"
 
 // project components
 import { MaterialUIPickers } from "@/components/Pickers/DatePicker"
@@ -820,6 +825,40 @@ export const SearchTickets = ({ type='searchPage', info=null }) => {
           <Grid item xs={12} sm={12} md={3}
                 className={`${classes.gridSelect}`}
           >
+            <LocalizationProvider
+              dateAdapter={AdapterDateFns}
+              adapterLocale={ruLocale}
+            >
+              <DatePicker
+                label="Дата поездки"
+                value={new Date()}
+                onChange={() => {}}
+                minDate={new Date()}
+                // className={
+                //   `${styles.date_picker__container} ${classNames}`
+                // }
+                // ref={refDatePicker}
+                // onClose={() => setIsOpen(false)}
+                // open={isOpen}
+                // onOpen={() => setIsOpen(true)}
+                componentsProps={{
+                  textField: {
+                    // WHY it isn't works?
+                    InputProps: {
+                      // ...InputProps,
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <CalendarToday />
+                        </InputAdornment>
+                      ),
+                      endAdornment: null
+                    },
+                    // className: `${styles.date_picker__container} ${styles.date_picker__input} ${searchTicketsStyles.searchField} ${isLastElementInRow ? styles.last_el : ''}`,
+                    // onClick: (e) => setIsOpen(true),
+                  }
+                }}
+              />
+            </LocalizationProvider>
             {/*<MaterialUIPickers*/}
             {/*  value={formData.journeyDate}*/}
             {/*  minDate={currentDateObj}*/}
