cd64a4540c23a6c6534a2b248a61a879db183422
harddownloader <serafimdev@gmail.com>
2023-06-22 01:14:29 +0300

    updated Home page: comment all getServerSideProps again

--- pages/index.tsx
+++ pages/index.tsx
@@ -55,39 +55,39 @@ function App({ locations=[] }: InferGetServerSidePropsType<typeof getServerSideP
 * Because Vercel has a timeout of 10 seconds, and our backend can only start at the time of the request.
 * As a result, we will get an error from Vercel.
 * */
-export const getServerSideProps: GetServerSideProps<{
-  locations: TLocations
-}> = async (context) => {
-  const { query, res } = context
-  res.setHeader(
-    'Cache-Control',
-    'public, s-maxage=10, stale-while-revalidate=59'
-  )
-
-  const props: {
-    locations: TLocations
-  } = {
-    locations: []
-  }
-
-  // try {
-  //   const timeout = 1000
-  //   const controller = new AbortController()
-  //   const id = setTimeout(() => controller.abort(), timeout)
-  //   const locations = await getAllLocations({
-  //     fetchOptions: {
-  //       signal: controller.signal
-  //     }
-  //   })
-  //   clearTimeout(id)
-  //   if (locations) props.locations = [...locations]
-  // } catch (error) {
-  //   console.error('Home page ssr fetching error', error)
-  // }
-
-  return {
-    props: props
-  }
-}
-
-export default App
+// export const getServerSideProps: GetServerSideProps<{
+//   locations: TLocations
+// }> = async (context) => {
+//   const { query, res } = context
+//   res.setHeader(
+//     'Cache-Control',
+//     'public, s-maxage=10, stale-while-revalidate=59'
+//   )
+//
+//   const props: {
+//     locations: TLocations
+//   } = {
+//     locations: []
+//   }
+//
+//   // try {
+//   //   const timeout = 1000
+//   //   const controller = new AbortController()
+//   //   const id = setTimeout(() => controller.abort(), timeout)
+//   //   const locations = await getAllLocations({
+//   //     fetchOptions: {
+//   //       signal: controller.signal
+//   //     }
+//   //   })
+//   //   clearTimeout(id)
+//   //   if (locations) props.locations = [...locations]
+//   // } catch (error) {
+//   //   console.error('Home page ssr fetching error', error)
+//   // }
+//
+//   return {
+//     props: props
+//   }
+// }
+//
+// export default App
